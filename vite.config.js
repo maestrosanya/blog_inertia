4cbf528654ce16403ba875591de6b7aef8e09886
maestrosanya <maestrosanya@yandex.ru>
2024-03-09 17:53:48 +0300

    add server settings

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 'resources/js/app.js',
+
+                'resources/css/admin/app.css', 'resources/js/admin/app.js'
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
